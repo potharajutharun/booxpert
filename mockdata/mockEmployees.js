4ce99e818c5f5297274f900b20c88bbb250e7687
techno-communications11 <ptharun@techno-communications.com>
2026-01-05 13:51:16 +0530

    bookx

---
+++ mockdata/mockEmployees.js
@@ -0,0 +1,18 @@
+export const mockEmployees = [
+  {
+    id: 1,
+    name: "Rahul Sharma",
+    gender: "Male",
+    dob: "1998-05-10",
+    state: "Telangana",
+    active: true
+  },
+  {
+    id: 2,
+    name: "Priya Verma",
+    gender: "Female",
+    dob: "1999-09-15",
+    state: "Karnataka",
+    active: false
+  }
+];
